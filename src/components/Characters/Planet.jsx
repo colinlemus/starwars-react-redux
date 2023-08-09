8f0a3cc4a31145dba9093230fd9f67f1051a4aad
Colin Lemus <colin@thelemus.com>
2023-08-08 23:22:06 -0700

    todo hashmap planet caching

--- src/components/Characters/Planet.jsx
+++ src/components/Characters/Planet.jsx
@@ -6,14 +6,27 @@ import pinImage from '../../assets/images/pin.png';
 export default function Planet(props) {
   const [planetName, setPlanetName] = useState("");
   const [loading, setLoading] = useState(true);
+  const planetCache = new Map();
+
+  //TODO:
+  //collect list of planets all 1 call and add to hashmap
+  //use hashmap to get planet name
 
   useEffect(() => {
-    axios.get(props.characterData.homeworld)
-      .then(data => {
-        setPlanetName(data.data.name);
-        setLoading(false);
-      })
-  }, []);
+    console.log(planetCache);
+    if (planetCache.has(props.characterData.homeworld)) {
+      setPlanetName(planetCache.get(props.characterData.homeworld));
+      setLoading(false);
+    } else {
+      axios.get(props.characterData.homeworld)
+        .then(data => {
+          setPlanetName(data.data.name);
+          setLoading(false);
+          // Save data to cache
+          planetCache.set(props.characterData.homeworld, data.data.name);
+        })
+    }
+  }, []); 
 
   let loadingPlanet = loading ? (<>Loading Planet...</>) : (planetName);
 
